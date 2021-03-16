f0d2cf48a5556f0b0f545289a63fd2ebe79dbc84
ARDIAN HILMI PRAMULINTANG <74774957+ardianhilmip35@users.noreply.github.com>
2021-03-16 10:47:23 +0700

    Add files via upload

---
+++ tugasketiga.java
@@ -0,0 +1,18 @@
+public class tugasketiga {
+    public static void main(String[] args) {
+        //PENULISAN VARIABEL BENAR
+        boolean gameOver = false;
+        int student = 50, classes = 3;
+        double sales_tax;
+        short number1;
+
+        //PENULISAN VARIABEL SALAH
+        int 2beOrNot2be;
+        float price index;
+        double lastYear'sPrice;
+        long class;
+
+
+
+    }
+}
