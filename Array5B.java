b67be039b3372b2b2cbebc5c10109f2b0eee5e07
ARDIAN HILMI PRAMULINTANG <74774957+ardianhilmip35@users.noreply.github.com>
2021-03-23 09:49:16 +0700

    tugas minggu ketiga

---
+++ Array5B.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Array5B {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int jumlahderet = 0;
+        System.out.print("Masukkan Jumlah Deret = ");
+        jumlahderet = input.nextInt();
+        int [] deretarray = new int [jumlahderet];
+
+        for(int i =0; i < jumlahderet; i++){
+            deretarray[i]=(int) (Math.random() * 10);
+        }
+
+        for(int j = 0; j < jumlahderet; j++){
+            System.out.print(deretarray[j]+"\t");
+    }
+    }
+}
---
+++ ArrayNama5A.java
@@ -0,0 +1,9 @@
+public class ArrayNama5A {
+    public static void main(String[] args) {
+        String[] nama ={"Ardian", "Hilmi", "Pramulintang"};
+        for (int i=0; i<nama.length; i++){
+            System.out.print(nama [i]);
+            System.out.print(" ");
+        }
+    }
+}
