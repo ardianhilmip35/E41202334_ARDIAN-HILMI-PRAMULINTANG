5fb30965616246b61779fab1c4b5e0fb40c2f3bd
ARDIAN HILMI PRAMULINTANG <74774957+ardianhilmip35@users.noreply.github.com>
2021-03-29 22:39:13 +0700

    Add files via upload

---
+++ tugas minggu keempat/src/BilanganKelipatanDua.java
@@ -0,0 +1,14 @@
+public class BilanganKelipatanDua {
+    public static void main(String[] args) {
+
+        System.out.println("           DO__WHILE        ");
+        System.out.println("Bilangan kelipatan 2 (1-100)");
+        System.out.println("============================");
+        int i = 1;
+        do{
+            System.out.print(i + "   ");
+            i += i;
+        }while(i<=100);
+
+    }
+}
