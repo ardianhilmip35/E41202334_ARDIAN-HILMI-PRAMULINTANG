62f9a3147c6008cbb891218f76bfb73d57329843
ARDIAN HILMI PRAMULINTANG <74774957+ardianhilmip35@users.noreply.github.com>
2021-04-05 12:53:48 +0700

    Update BilanganKelipatanDua.java

--- tugas minggu keempat/src/BilanganKelipatanDua.java
+++ tugas minggu keempat/src/BilanganKelipatanDua.java
@@ -2,7 +2,7 @@ public class BilanganKelipatanDua {
     public static void main(String[] args) {
 
         System.out.println("           DO__WHILE        ");
-        System.out.println("Bilangan kelipatan 2 (1-100)");
+        System.out.println("Bilangan Perkalian 2 (1-100)");
         System.out.println("============================");
         int i = 1;
         do{
